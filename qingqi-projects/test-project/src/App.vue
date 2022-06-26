1d3e5d4db3e0def26f559d7ab637dc073f23c72a
yunfei.wang <yunfei.wang@easydatalink.com>
2022-06-26 19:35:35 +0800

    6

--- qingqi-projects/test-project/src/App.vue
+++ qingqi-projects/test-project/src/App.vue
@@ -1,20 +1,10 @@
-<script setup lang="ts">
+<script setup>
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 </script>
 
 <template>
-<div>
-  <!-- 左 -->
-  <div>菜单</div>
-  <!-- 右 -->
-  <div>
-    <!-- head -->
-    <div class=""></div>
-    <!-- content -->
-    <div></div>
-  </div>
-</div>
+  你好
 </template>
 
 <style>
