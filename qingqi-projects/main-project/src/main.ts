c98a4b1922cd0ba73d51fb2e99b9379fc1342862
yunfei.wang <yunfei.wang@easydatalink.com>
2022-06-26 11:17:56 +0800

    1

--- qingqi-projects/main-project/src/main.ts
+++ qingqi-projects/main-project/src/main.ts
@@ -1,4 +1,25 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+
+import { registerMicroApps, start } from 'qiankun';
+
+registerMicroApps([
+    {
+        name: 'reactApp',
+        entry: '//localhost:3000',
+        container: '#container',
+        activeRule: '/app-react',
+    },
+    {
+        name: 'vueApp',
+        entry: '//localhost:8080',
+        container: '#container',
+        activeRule: '/app-vue',
+    },
+]);
+// 启动 qiankun
+start();
+
+
 createApp(App).mount('#app')
