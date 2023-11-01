e40f7cd070bf9c6d0315a882974412dedc261b36
yunfei.wang <1669136244@qq.com>
2023-11-01 12:04:39 +0800

    list类型创建api

--- qingqi-uicg/src/index.ts
+++ qingqi-uicg/src/index.ts
@@ -3,26 +3,30 @@
  * @Author: yunfei
  * @Date: 2023-10-13 16:55:31
  */
-// import { render, renderString, compile } from 'nunjucks';
-import nunjucks from 'nunjucks';
-import { writeFileSync, existsSync, readFileSync, mkdirSync, readdir } from 'fs';
+// import { render, renderString, compile } from 'nunjucks'
+import nunjucks from 'nunjucks'
+import { writeFileSync, existsSync, readFileSync, mkdirSync, readdir } from 'fs'
 import { resolve, join } from 'path'
-import { CMD } from './types/cmd';
-import { ParseCmd } from './class/ParseCmd';
+import { CMD } from './types/cmd'
+import { ParseCmd } from './class/ParseCmd'
 import chalk from 'chalk'
 
 export class CreatePage {
     constructor(private cmd: CMD) {
-        this.run(new ParseCmd(cmd));
+        this.run(new ParseCmd(cmd))
     }
 
     private async run(cmd: ParseCmd) {
 
-        nunjucks.configure(join(__dirname, '../template/'), { autoescape: true });
+        nunjucks.configure(join(__dirname, '../template/'), { autoescape: true })
         nunjucks.render(cmd.getFeature() + '.ejs', cmd.getCmd(), (err, res) => {
             this.write(res, cmd)
         })
-        this.createApi(cmd);
+
+        // list == List
+        if (cmd.getFeature() == 'List') {
+            this.createApi(cmd)
+        }
     }
     // api
     private createApi(cmd: ParseCmd) {
@@ -30,22 +34,27 @@ export class CreatePage {
         this.findPath(toFile).then((res: string) => {
             nunjucks.render('./api/index.js', cmd.getCmd(), (err, content) => {
                 if (!err) {
-                    writeFileSync(resolve(res, `${cmd.getApiName()}.js`), content, 'utf-8');
-                    // 写入语法
-                    // //##import //##export
-                    let txt = readFileSync(resolve(res, `index.js`), 'utf-8')
-                    txt = txt.replace('//##import', `//##import 
-                    import ${cmd.getApiName()} from '@/api/${cmd.getApiName()}.js'`)
-                    txt = txt.replace('//##export', `//##export
-                    ${cmd.getApiName()},`)
+                    try {
 
+                        writeFileSync(resolve(res, `${cmd.getApiName()}.js`), content, 'utf-8')
+                        let txt = readFileSync(resolve(res, `index.js`), 'utf-8')
+                        txt = txt.replace('//##import', `import ${cmd.getApiName()} from '@/api/${cmd.getApiName()}.js'
+                        //##import
+                        `)
+                        txt = txt.replace('//##export', `${cmd.getApiName()},
+                        //##export
+                        `)
 
-                    writeFileSync(resolve(res, `index.js`), txt, 'utf-8');
+                        writeFileSync(resolve(res, `index.js`), txt, 'utf-8')
+                    } catch (err) {
+                        console.log(chalk.red('api【index.js】 file not found'))
+                        console.log('info', err)
+                    }
                 }
             })
 
         }).catch(err => {
-            console.log(chalk.red(err));
+            console.log(chalk.red(err))
         })
 
     }
@@ -57,7 +66,7 @@ export class CreatePage {
                         // 在
                         res(resolve(path, 'api'))
                     } else {
-                        console.log('path', path);
+                        console.log('path', path)
                         // 当前是否在根目录
                         if (path == resolve(path, '../')) {
                             rej('目录【/api】不存在，无法生成接口')
@@ -84,10 +93,10 @@ export class CreatePage {
             if (!existsSync(to)) {
                 mkdirSync(to)
             }
-            writeFileSync(toFile, content, 'utf-8');
+            writeFileSync(toFile, content, 'utf-8')
         } else {
             let toFile = resolve(`${cmd.getCwd()}.${cmd.getFile()}`)
-            writeFileSync(toFile, content, 'utf-8');
+            writeFileSync(toFile, content, 'utf-8')
         }
     }
 }
